91819ec3d6e90b00ea44f099ec87b21e9ed8ded4
NEETroid <7darkza9@gmail.com>
2021-12-23 10:02:06 +0700

    update
    Signed-off-by: NEETroid <7darkza9@gmail.com>

--- checker.sh
+++ checker.sh
@@ -2,49 +2,65 @@
 # Created by ZyCromerZ
 sudo apt-get install -y curl git
 
-ForA="$(cat 4.4)"
-ForB="$(cat 4.14)"
+ForA="$(cat 4.4-common)"
+ForB="$(cat 4.14-common)"
+ForC="$(cat 4.4)"
+ForD="$(cat 4.14)"
+
+git config user.name NEETroid
+git config user.email 7darkza9@gmail.com
+
+git add . && git commit -sm 'update for next notif' && git push -f --all
 
 SendInfo(){
+    if [[ "$2" == "gugel" ]];then
+        link="https://android.googlesource.com/kernel/common"
+    else
+        link="https://git.kernel.org/pub/scm/linux/kernel/git/stable/linux.git"
+    fi
     curl -s -X POST "https://api.telegram.org/bot${BOT_TOKEN}/sendMessage" \
     -d chat_id="-1001400256602" \
     -d "disable_web_page_preview=true" \
     -d "parse_mode=html" \
-    -d text="Kernel Tags ${1} Ready on https://android.googlesource.com/kernel/common, when upstream?"
+    -d text="Kernel Tags ${1} Ready on $link, when upstream?"
 }
 Total="0"
-RunCheck()
-{
+while [[ "$Total" != "58" ]];do
     if [[ -z "${BOT_TOKEN}" ]];then
         echo "bot token null"
-        exit 0
+        exit
     else
         wget https://android.googlesource.com/kernel/common/+/refs/heads/android-4.4-p/Makefile -O A.txt
         wget https://android.googlesource.com/kernel/common/+/refs/heads/android-4.14-stable/Makefile -O B.txt
+        wget https://git.kernel.org/pub/scm/linux/kernel/git/stable/linux.git/plain/Makefile?h=linux-4.4.y -O C.txt
+        wget https://git.kernel.org/pub/scm/linux/kernel/git/stable/linux.git/plain/Makefile?h=linux-4.14.y -O D.txt
 
         if [[ ! -z "$(cat A.txt | grep '<span class="lit">'$ForA'</span>')" ]];then
-            SendInfo "4.4.${ForA}"
+            SendInfo "4.4.${ForA}" "gugel"
             ForA=$(($ForA+1))
-            echo $ForA > 4.4
+            echo $ForA > 4.4-common
         fi
 
         if [[ ! -z "$(cat B.txt | grep '<span class="lit">'$ForB'</span>')" ]];then
-            SendInfo "4.14.${ForB}"
+            SendInfo "4.14.${ForB}" "gugel"
             ForB=$(($ForB+1))
-            echo $ForB > 4.14
+            echo $ForB > 4.14-common
         fi
-        rm -rf A.txt B.txt
+        if [[ ! -z "$(cat C.txt | grep "SUBLEVEL = $ForC")" ]];then
+            SendInfo "4.4.${ForC}" "std"
+            ForC=$(($ForC+1))
+            echo $ForC > 4.4
+        fi
+
+        if [[ ! -z "$(cat D.txt | grep "SUBLEVEL = $ForD")" ]];then
+            SendInfo "4.14.${ForD}" "std"
+            ForD=$(($ForD+1))
+            echo $ForD > 4.14
+        fi
+        rm -rf A.txt B.txt C.txt D.txt
     fi
-    sleep 30s
-    if [[ "$Total" == "28" ]];then
-        exit
-    fi
+    sleep 60s
     Total=$(($Total+1))
-    RunCheck
-}
-RunCheck
-
-git config --global user.name NEETroid
-git config --global user.email 7darkza9@gmail.com
-
-git add . && git commit -sm 'update for next notif' && git push -f --all
+    git add . && git commit -sm 'update for next notif'
+    git push -f --all
+done
